5a120c4e4127f96fbab5613abb566f70b93bfb07
Ersandeko <99676562+Ersandeko@users.noreply.github.com>
2023-01-11 10:29:26 +0100

    Add files via upload

---
+++ controller.js
@@ -0,0 +1,43 @@
+app.controller('InstantSearchController', function($scope){
+
+
+	$scope.items = [
+		{
+			url: 'http://tutorialzine.com',
+			title: 'NodeJs tutorials',
+			image: 'https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+		},
+		{
+			url: 'http://tutorialzine.com',
+			title: 'AngularJs Tutorials ',
+			image: 'https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+		},
+		{
+			url: 'http://tutorialzine.com',
+			title: 'How to Make a Digital Clock with jQuery',
+			image: 'https://media.istockphoto.com/id/1410581089/photo/business-woman-with-finger-pressing-jquery-button-on-virtual-screens.jpg?s=612x612&w=is&k=20&c=ghMqAFS7WD3IZ4F3VDJksPGUiKDolfRtlkZ69-gkExw='
+		},
+		{
+			url: 'http://tutorialzine.com',
+			title: '50 Must-have plugins for extending Twitter Bootstrap',
+			image: 'http://tutorialzine.com/media/2013/07/featured_4.jpg'
+		},
+		{
+			url: 'http://tutorialzine.com',
+			title: 'Smooth Diagonal Fade Gallery with CSS3 Transitions',
+			image: 'http://tutorialzine.com/media/2013/05/featured.jpg'
+		},
+		{
+			url: 'http://tutorialzine.com',
+			title: 'Mini AJAX File Upload Form',
+			image: 'http://tutorialzine.com/media/2013/05/ajax-file-upload-form.jpg'
+		},
+		{
+			url: 'http://tutorialzine.com',
+			title: 'Your First Backbone.js App - Service Chooser ',
+			image: 'http://tutorialzine.com/media/2013/04/service_chooser_form.jpg'
+		}
+	];
+
+
+})
